098a81c12f46d72176a9218895ae0df1d4bc1b23
Integr-0 <erik.reitbauer@outlook.de>
2024-11-18 13:28:00 +0100

    Utils

--- src/main/kotlin/net/integr/castl3d/Constants.kt
+++ src/main/kotlin/net/integr/castl3d/Constants.kt
@@ -23,6 +23,21 @@ class Constants {
             const val QUEEN = 4
             const val KING = 5
             const val NONE = -1
+
+            /**
+             * Returns the value of the piece.
+             */
+            fun Int.pieceValue(kingValue: Int = 1000): Int {
+                return when (this) {
+                    PAWN -> 1
+                    KNIGHT -> 3
+                    BISHOP -> 3
+                    ROOK -> 5
+                    QUEEN -> 9
+                    KING -> kingValue
+                    else -> 0
+                }
+            }
         }
     }
 
